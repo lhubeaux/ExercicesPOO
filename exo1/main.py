ae5d68babe84dc7ea5682946004f49a2b13517eb
lhubeaux <louis.hubeaux@gmail.com>
2026-05-19 16:14:52 +0200

    sorti les objets de la fonction

--- exo1/main.py
+++ exo1/main.py
@@ -6,13 +6,17 @@ eleph_1.definir_elephant("Jean-Michel", 85, 90, "personne")
 eleph_2.definir_elephant("Albertine", 50, 90, "personne")
 
 
+soigneur_1 = Soigneur()
+soigneur_1.definir_soigneur("Fred", "25-10-1990", 10)
+
+enclos_1 = Enclos()
+enclos_1.definir_enclos("Enclos 1", 10, 10)
+
 def main():
 
     print(eleph_1.afficher_elephant())
     print(eleph_2.afficher_elephant())
 
-    soigneur_1 = Soigneur()
-    soigneur_1.definir_soigneur("Fred", "25-10-1990", 10)
 
     print(soigneur_1.ajouter_animal(eleph_1))
     print(soigneur_1.nourrir(eleph_1))
@@ -22,9 +26,6 @@ def main():
     print(soigneur_1.nourrir(eleph_2))
     print(soigneur_1.entretenir(eleph_2))
 
-
-    enclos_1 = Enclos()
-    enclos_1.definir_enclos("Enclos 1", 10, 10)
     enclos_1.ajouter_animal(eleph_1)
     enclos_1.ajouter_animal(eleph_2)
     print(enclos_1.afficher_animaux())
