512a767c57a45502bf49c9d6b0f1582cc39b0c15
lhubeaux <louis.hubeaux@gmail.com>
2026-05-22 16:45:07 +0200

    fini exos faire vérifs

---
+++ exo3/models/__init__.py
@@ -0,0 +1,13 @@
+from .elephant import Elephant
+from .soigneur import Soigneur
+from .enclos import Enclos
+from .giraffe import Giraffe
+from .animal import Animal
+
+__all__ = [
+	"Elephant",
+	"Soigneur",
+	"Enclos",
+    "Giraffe",
+    "Animal"
+]
