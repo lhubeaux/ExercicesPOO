ac26def84372eeea7bde9094a684e5643227cd5a
lhubeaux <louis.hubeaux@gmail.com>
2026-05-26 16:10:19 +0200

    exos statiques

---
+++ Exos/exo6 (membres statiques)/models/__init__.py
@@ -0,0 +1,15 @@
+from .elephant import Elephant
+from .soigneur import Soigneur
+from .enclos import Enclos
+from .giraffe import Giraffe
+from .animal import Animal
+from .consoletools import Outils
+
+__all__ = [
+	"Elephant",
+	"Soigneur",
+	"Enclos",
+    "Giraffe",
+    "Animal",
+    "Outils"
+]
